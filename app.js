e42c2ba00f69f7193b9070ae4672c05ea2971f70
asaulters <71015650+asaulters@users.noreply.github.com>
2021-10-25 21:26:11 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', 'views')
+
+const indexController = require('./routes/index');
+
+app.use('/', indexController)
+
+app.listen(3000, console.log('listening to port 3000'))
