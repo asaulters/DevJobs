0e042cbd42265debfc34fb65ae3621e8a92e220b
asaulters <71015650+asaulters@users.noreply.github.com>
2021-11-03 21:45:02 -0400

    Add files via upload

--- controller/jobs.js
+++ controller/jobs.js
@@ -1,28 +1,67 @@
 const fs = require('fs');
+const path = require('path')
 const { exit } = require('process');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data.json'
+);
 
 // const jobListSection = document.querySelector('.job-list');
 
-
-exports.getJobs = (req, res, next) => {
-    res.render('../view/index')
-    const jobs = loadJobs();
-    jobs.forEach(jobs => {
-        // console.log(jobs.position)
-        let jobDiv = document.createElement('div');
-        let newJobDiv = 
-    });
-    // console.log(strJob.company)
-    // // console.log(jobs)
-    // for(x = 0; x < jobs.length; x++) {
-        // let jobEach = jobs;
-    //     // let newJob = document.createElement('div');
-    //     // const newJobs = jobs.toString();
-    //     // console.log(newJobs)
-
-    // }
+class Job {
+    constructor (id, company, logo, logoBackground, position, postedAt, contract, location, website, apply, description, requirements, role){
+        this.id =  id,
+        this.company = company,
+        this.logo = logo,
+        this.logoBackground = logoBackground, 
+        this.position = position, 
+        this.postedAt = postedAt,
+        this.contract = contract,
+        this.location = location,
+        this.website = website, 
+        this.apply = apply, 
+        this.description= description,
+        this.requirements = requirements,
+        this.role = role
+    }
 }
 
+
+
+
+
+
+// id =  id,
+// company = company,
+// logo = logo,
+// logoBackground = logoBackground, 
+// position = position, 
+// postedAt = postedAt,
+// contract = contract,
+// location = location,
+// website = website, 
+// apply = apply, 
+// description= description,
+// requirements = requirements,
+// role = role
+
+exports.getJobs = (req, res, next) => {
+    Job.fetchAll((products)=> {
+        res.render('../view/index', {
+            jobs: jobs
+        })
+        console.log(jobs)
+    })
+    // const jobs = loadJobs();
+
+    // for (x = 0; x < jobs.length; x++) {
+    //     // console.log(jobs[x].position)
+
+    //         let newJob =new Job(jobs[x].id, jobs[x].company, jobs[x].logo, jobs[x].logoBackground, jobs[x].position, jobs[x].postedAt, jobs[x].contract, jobs[x].location, jobs[x].website, jobs[x].apply, jobs[x].description, jobs[x].requirements, jobs[x].role )
+    //         console.log(newJob)
+    // }
+}
 
 
 
@@ -38,5 +77,5 @@ const loadJobs = function(res) {
         console.log(e) 
 
     }
-}
+};
 
